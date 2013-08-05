20106159299feb6818d4baf93ac66c07fc604c2c
Qiu Juntao <juntao.qiu@gmail.com>
2013-08-05 17:27:18 +0800

    classify the script

--- turbo.rb
+++ turbo.rb
@@ -1,45 +1,55 @@
 require 'json'
-
 require './hash_recursive_merge.rb'
 
-def generate_header(obj)
-	headers = []
-	obj.each_pair do |k, v|
-		headers << "-H \"#{k}: #{v}\""
+class Turbo
+	def initialize(conf="turbo.conf")
+		@conf = JSON.parse(File.read(conf))
+		p @conf
 	end
-	headers.join(' ')
-end
 
-def load_common
-	JSON.parse(File.read('common.conf'))
-end
+	def run
+		Dir.glob(@conf['scenarios_path']) do |json_file|
+			run_scenario(json_file)
+		end
+	end
 
-def load_scenario(scenario)
-	JSON.parse(File.read(scenario))
-end
+	private
 
-def run_scenario(scenario)
-	common = load_common
-	config = common.rmerge(load_scenario(scenario))
+	def generate_header(obj)
+		headers = []
+		obj.each_pair do |k, v|
+			headers << "-H \"#{k}: #{v}\""
+		end
+		headers.join(' ')
+	end
 
-	# generate all headers
-	headers = generate_header(config['headers'])
-	
-	# generate HTTP method
-	method = "-X #{config['method']}"
+	def load_common
+		JSON.parse(File.read(@conf['common_conf']))
+	end
 
-	# run each case here
-	config['cases'].each do |caze|
-		path = config['baseurl'] + caze['path']
-		data = config['method'] == "POST" ? "-d @#{caze['data']}" : ""
-		command = "curl -is #{headers} #{method} #{data} #{path}"
-		puts "#{config['method']} #{path}"
-		system "#{command} | ack \"#{caze['success']}\"; $? == 0 ? echo 'success' : 'failed'"
+	def load_scenario(scenario)
+		JSON.parse(File.read(scenario))
+	end
+
+	def run_scenario(scenario)
+		common = load_common
+		config = common.rmerge(load_scenario(scenario))
+
+		# generate all headers
+		headers = generate_header(config['headers'])
+		
+		# generate HTTP method
+		method = "-X #{config['method']}"
+
+		# run each case here
+		config['cases'].each do |caze|
+			path = config['baseurl'] + caze['path']
+			data = config['method'] == "POST" ? "-d @#{caze['data']}" : ""
+			command = "curl -is #{headers} #{method} #{data} #{path}"
+			puts "#{config['method']} #{path}"
+			system "#{command} | ack \"#{caze['success']}\""
+		end
 	end
 end
 
-def main
-	run_scenario('search.json')
-end
-
-main
+Turbo.new.run
